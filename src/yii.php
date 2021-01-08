57ef9b846cc6b0639c7454fb99548907f910c4d3
Charles DELFLY <c.delfly@linkbynet.com>
2021-01-08 13:54:36 +0700

    Use pinpoint-core-plugin instead of duplicating it.

--- src/yii.php
+++ src/yii.php
@@ -12,12 +12,12 @@ define('PINPOINT_USE_CACHE', (defined('YII_ENV') && YII_ENV == "dev") ? 'NO' : '
 define('AOP_CACHE_DIR', __DIR__ . '/../../../../runtime/pinpoint/cache/');
 
 // Plugins directory
-define('PLUGINS_DIR', __DIR__ . '/plugins/');
+define('PLUGINS_DIR', __DIR__ . '/Plugins/');
 
 // Application id and name to appear in pinpoint UI
 define('APPLICATION_ID', isset($config['id']) ? $config['id'] : 'yii' );
 define('APPLICATION_NAME', isset($config['name']) ? $config['name'] : APPLICATION_ID);
 
-define('PP_REQ_PLUGINS', '\Plugins\Yii2ReqPlugins');
+define('PP_REQ_PLUGINS', '\Plugins\Common\PerRequestPlugins');
 
 require_once __DIR__ . '/../../../pinpoint-apm/pinpoint-php-aop/auto_pinpointed.php';
