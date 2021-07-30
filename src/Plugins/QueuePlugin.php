934cd693bc6f5c2e8b3f2796dcc101d161a3e9ed
Charles DELFLY <c.delfly@linkbynet.com>
2021-07-30 13:01:44 +0700

    Fix flush issue for queue plugin

--- src/Plugins/QueuePlugin.php
+++ src/Plugins/QueuePlugin.php
@@ -5,17 +5,23 @@ namespace Plugins;
 use pinpoint\PluginsCore\Common\Candy;
 
 ///@hook:yii\queue\cli\Queue::handleMessage
+///@hook:yii\queue\cli\SignalLoop::canContinue
 class QueuePlugin extends Candy
 {
     public function __construct($apId, $who, &...$args)
     {
         $argv = isset($_SERVER['argv']) ? implode(" ", $_SERVER['argv']) : '';
-
         // Special behavior for never ending worker
         if (preg_match('/\/listen.+--isolate\s+0/', $argv) === 1)
         {
-            PerRequestPlugins::instance()->sendTrace();
-            pinpoint_add_clue(PP_REQ_URI, sprintf("[id:%s] %s", $args[0], $argv));
+            if ($apId == "yii\queue\cli\SignalLoop::canContinue")
+            {
+                PerRequestPlugins::instance()->sendTrace();
+            }
+            else
+            {
+                pinpoint_add_clue(PP_REQ_URI, sprintf("[id:%s] %s", $args[0], $argv));
+            }
         }
 
         array_unshift($args, $apId, $who);
@@ -25,7 +31,10 @@ class QueuePlugin extends Candy
     function onBefore()
     {
         pinpoint_add_clue(PP_SERVER_TYPE, PP_PHP_METHOD);
-        pinpoint_add_clues(PP_PHP_ARGS, sprintf("[id:%s][ttr:%s][attempt:%s]", $this->args[0], $this->args[2], $this->args[3]));
+        if ($this->apId != "yii\queue\cli\SignalLoop::canContinue")
+        {
+            pinpoint_add_clues(PP_PHP_ARGS, sprintf("[id:%s][ttr:%s][attempt:%s]", $this->args[0], $this->args[2], $this->args[3]));
+        }
     }
 
     function onEnd(&$ret)
